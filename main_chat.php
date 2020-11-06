7bde768647672723302d2746bfe08d4697b11152
lyes <lyes.khalfaoui@dataimpact.fr>
2020-11-06 10:44:52 +0100

    working on css

--- main_chat.php
+++ main_chat.php
@@ -23,7 +23,7 @@ on_load();
 
 
     <div class=header>
-        <div id="pseudo"> <?php echo($_SESSION['pseudo']); ?></div>
+        <div id="pseudo"> <p> <?php echo( "Connecté en tant que" + $_SESSION['pseudo']); ?></p></div>
 
         <form action="deconnexion.php" method="post">
     <input type="submit" class="headers_button" name="deco" value="Se deconnecter" />
