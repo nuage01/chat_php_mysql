9ad26f41b747d430d03ac2ec784719e84a530fa5
lyes <lyes.khalfaoui@dataimpact.fr>
2020-11-09 20:05:24 +0100

    testing poo on bdd class

--- user.class.php
+++ user.class.php
@@ -15,7 +15,7 @@ class User
     public function display_infos(){
         $base = DBConnexion::getInstance();
         $reponse = $base->vars_query('SELECT * FROM  USERS where LOGIN = (?)', array($this->login));
-        return $response
+        return $response;
     }
 
 }
